7ccb585800b5ee3073bc15e4dd7b2e8469f31a3d
itsparasbisht <iamparasbisht@gmail.com>
2022-06-29 20:39:45 +0530

    link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href="/about">About</Link> <br />
+      <Link href="/product">Products</Link>
+    </>
+  );
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 function Product() {
-  return <h1>Product Page</h1>;
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>
+        <Link href="/product/1">Product 1</Link>
+      </h1>
+      <h1>
+        <Link href="/product/2">Product 2</Link>
+      </h1>
+      <h1>
+        <Link href="/product/3" replace>
+          Product 3
+        </Link>
+      </h1>
+    </>
+  );
 }
 
 export default Product;

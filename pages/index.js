78f02f46b82da5863991a2201277114dcbec06cb
itsparasbisht <iamparasbisht@gmail.com>
2022-07-02 20:49:25 +0530

    router.push

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/product");
+    // router.replace("/product");
+  };
+
   return (
     <>
       <h1>Home Page</h1>
       <Link href="/about">About</Link> <br />
-      <Link href="/product">Products</Link>
+      <Link href="/product">Products</Link> <br />
+      <button onClick={handleClick}>Buy Products</button>
     </>
   );
 }

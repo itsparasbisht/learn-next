ef24c9420c1496e84c4f3a033a3cead0d6828049
itsparasbisht <iamparasbisht@gmail.com>
2022-07-02 20:40:11 +0530

    a comment

--- pages/docs/[[...params]].js
+++ pages/docs/[[...params]].js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 
+// [[]] - double brackets allows to catch /docs also + /docs/something/.../.../...
+
 function Docs() {
   const route = useRouter();
   const { params } = route.query;

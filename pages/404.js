e2ed4d37baff5ac3a7f82ac36b5aad1f2415a160
itsparasbisht <iamparasbisht@gmail.com>
2022-07-03 11:37:01 +0530

    remove files

--- pages/404.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NotFound() {
-  return <div>My NotFound Page</div>;
-}
-
-export default NotFound;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
 function Home() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push("/product");
-    // router.replace("/product");
-  };
-
-  return (
-    <>
-      <h1>Home Page</h1>
-      <Link href="/about">About</Link> <br />
-      <Link href="/product">Products</Link> <br />
-      <button onClick={handleClick}>Buy Products</button>
-    </>
-  );
+  return <h1>Next JS pre-rendering</h1>;
 }
 
 export default Home;

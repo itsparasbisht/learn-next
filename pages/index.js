30de0933dc201bf4c2c4e85cf10cd6d1801cc9a1
itsparasbisht <iamparasbisht@gmail.com>
2022-07-14 20:39:27 +0530

    api from here

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,7 @@ import Link from "next/link";
 function Home() {
   return (
     <>
-      <h1>Next JS pre-rendering</h1>
-      <Link href="/users">
-        <a>Users</a>
-      </Link>{" "}
-      <br />
-      <Link href="/posts">
-        <a>Posts</a>
-      </Link>
+      <h1>NextJs</h1>
     </>
   );
 }

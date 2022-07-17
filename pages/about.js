80319c892caca1ffa8e59c702bab1e9fb29dbff5
itsparasbisht <iamparasbisht@gmail.com>
2022-07-17 20:29:18 +0530

    head component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,19 @@
+import Head from "next/head";
 import Footer from "../components/Footer";
 
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+      <Head>
+        <title>About this page</title>
+        <meta
+          name="description"
+          content="this is the description of the current page"
+        />
+      </Head>
+      <h1>About Page</h1>
+    </>
+  );
 }
 
 About.getLayout = function PageLayout(page) {
